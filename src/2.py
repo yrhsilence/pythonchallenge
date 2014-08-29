05f010f4fa5a284ade14458b39904e203a73ae98
yrhsilence <yrhsilence@gmail.com>
2014-08-29 15:47:53 +0800

    add src/1,2,3

---
+++ src/2.py
@@ -0,0 +1,7 @@
+import string
+
+a = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+aa = "map"
+b = string.maketrans('abcdefghijklmnopqrstuvwxyz', 'cdefghijklmnopqrstuvwxyzab')
+print a.translate(b)
+print aa.translate(b)
